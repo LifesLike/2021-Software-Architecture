42fc1aee21da9cee7b242a8fcbe7904eb52d3860
kyuhyeongkwon <akcps1tp@naver.com>
2021-12-05 23:18:41 +0900

    prev command cancel removed

--- garageDoor/scheduler/PrioritySchedulingAlgorithm.java
+++ garageDoor/scheduler/PrioritySchedulingAlgorithm.java
@@ -7,17 +7,12 @@ import java.util.PriorityQueue;
 
 public class PrioritySchedulingAlgorithm implements SchedulingAlgorithm {
     private PriorityQueue<Command> queue = new PriorityQueue<>(Collections.reverseOrder());
-    private Command prevCommand = null;
 
     @Override
     public void addCommand(Command command) {
         queue.add(command);
         Command nextCommand = getNextCommand();
-        if (prevCommand != null) {
-            prevCommand.cancel();
-        }
         nextCommand.start();
-        prevCommand = nextCommand;
     }
 
     @Override
