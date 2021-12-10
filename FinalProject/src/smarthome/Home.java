ff5306973486441a0ee717df16a0aac7c4d2e833
Pastelgreener <94100684+Pastelgreener@users.noreply.github.com>
2021-12-10 21:16:05 +0900

    Add files via upload

---
+++ FinalProject/src/smarthome/Home.java
@@ -0,0 +1,47 @@
+package smarthome;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Home extends JPanel {
+    Color color = new Color(204, 255, 153, 200);
+    ImageIcon imgicon = new ImageIcon("img/UI_Mark.jpg");
+    Image img = imgicon.getImage();
+    private GUIFrame GF;
+
+    public void paintComponent(Graphics g) {
+        // TODO Auto-generated method stub
+        super.paintComponent(g);
+
+        g.drawImage(img, 80, 100, 130, 120, null);
+
+    }
+
+    public Home(GUIFrame GF) {
+        this.GF = GF;
+        JButton garageDoor = new JButton("GarageDoor");
+        JButton addService = new JButton("AddService");
+        JButton home = new JButton("Home");
+        setLayout(new FlowLayout());
+        setBackground(color);
+        add(garageDoor);
+        add(addService);
+        garageDoor.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                GF.change("garagedoor");
+
+            }
+        });
+        addService.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                GF.change("addservice");
+            }
+        });
+
+    }
+
+}
