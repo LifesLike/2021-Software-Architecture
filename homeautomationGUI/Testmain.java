7b892b535e0ec680b889f18080fecdec925ae749
Pastelgreener <94100684+Pastelgreener@users.noreply.github.com>
2021-12-10 18:00:45 +0900

    Add files via upload

---
+++ homeautomationGUI/Testmain.java
@@ -0,0 +1,9 @@
+package homeautomationGUI;
+
+public class Testmain {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        GUIFrame GF = new GUIFrame("7조 SA프로젝트 ");
+
+    }
+}
