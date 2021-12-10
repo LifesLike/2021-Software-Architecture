6a2a63144b3c2abcaec20b63d59b9565436c278b
Pastelgreener <94100684+Pastelgreener@users.noreply.github.com>
2021-12-10 21:27:20 +0900

    Add files via upload

---
+++ smarthome/DetectObstacle.java
@@ -0,0 +1,85 @@
+package FinalProject.smarthome;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+public class DetectObstacle extends JPanel {
+    Color color = new Color(204, 255, 153, 200);
+    ImageIcon imgicon = new ImageIcon("img/UI_Mark.jpg");
+    Image img = imgicon.getImage();
+    private GUIFrame GF;
+
+    public void paintComponent(Graphics g) {
+        // TODO Auto-generated method stub
+        super.paintComponent(g);
+
+        g.drawImage(img, 80, 100, 130, 120, null);
+
+    }
+
+    public DetectObstacle(GUIFrame GF) {
+        JPanel panel1 = new JPanel();
+
+        panel1.setBounds(0, 0, 300, 30);
+        panel1.setBackground(color);
+        panel1.setLayout(new FlowLayout());
+
+        JPanel panel2 = new JPanel();
+        panel2.setBounds(0, 30, 300, 30);
+        panel2.setBackground(color);
+        panel1.setLayout(new FlowLayout());
+
+        setLayout(null);
+        setSize(300, 300);
+        //setLayout(new FlowLayout());
+        setBackground(color);
+        ButtonGroup g = new ButtonGroup();
+        JRadioButton detected = new JRadioButton("Detected", false);
+        JRadioButton cleared = new JRadioButton("Cleared", false);
+        JButton back = new JButton("Back");
+        JButton home = new JButton("Home");
+
+        g.add(detected);
+        g.add(cleared);
+
+        panel1.add(detected);
+        panel1.add(cleared);
+        panel2.add(back);
+        panel2.add(home);
+
+        add(panel1);
+        add(panel2);
+
+        detected.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+
+            }
+        });
+        cleared.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+
+            }
+        });
+        back.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                GF.change("garagedoor");
+
+            }
+        });
+        home.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                GF.change("home");
+
+            }
+        });
+
+
+    }
+
+
+}
