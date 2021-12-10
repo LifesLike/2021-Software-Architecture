fc22ca72ecc55dcf0c3e0e77d145b362aca80ba4
Pastelgreener <94100684+Pastelgreener@users.noreply.github.com>
2021-12-10 21:17:11 +0900

    Delete FinalProject directory

--- FinalProject/src/smarthome/Testmain.java
+++
@@ -1,9 +0,0 @@
-package smarthome;
-
-public class Testmain {
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        GUIFrame GF = new GUIFrame("7조 SA프로젝트 ");
-
-    }
-}
