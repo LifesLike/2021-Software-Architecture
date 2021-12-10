68dddc21c7c3395f8cabd0a57809cf1a2bb053df
Pastelgreener <94100684+Pastelgreener@users.noreply.github.com>
2021-12-10 21:26:29 +0900

    Add files via upload

---
+++ smarthome/Testmain.java
@@ -0,0 +1,9 @@
+package FinalProject.smarthome;
+
+public class Testmain {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        GUIFrame GF = new GUIFrame("7조 SA프로젝트 ");
+
+    }
+}
