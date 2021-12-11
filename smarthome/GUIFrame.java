e36fb0f9f964059b0e15008fa21428991140104b
Pastelgreener <94100684+Pastelgreener@users.noreply.github.com>
2021-12-11 15:16:54 +0900

    Update GUIFrame.java

--- smarthome/GUIFrame.java
+++ smarthome/GUIFrame.java
@@ -1,20 +1,24 @@
 package FinalProject.smarthome;
 
+import FinalProject.smarthome.communication.Communication;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class GUIFrame extends JFrame {
+    private Communication communication;
     Container frame = this.getContentPane(); // 메인 컨테이너 객체
     Color color = new Color(204, 255, 153, 200);
     ImageIcon imgicon = new ImageIcon("img/UI_Mark.jpg");
     Image img = imgicon.getImage();
     Home H = new Home(this);
-    GarageDoor GD = new GarageDoor(this);
+    GarageDoor GD = new GarageDoor(this, communication);
     AddService AS = new AddService(this);
 
 
-    public GUIFrame(String title) {
+    public GUIFrame(String title, Communication communication) {
         super(title);
+        this.communication = communication;
         this.setSize(300, 300);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
