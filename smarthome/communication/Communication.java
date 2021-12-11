393c478b0b7ebfc4d478be2f89fac5359dd480ef
Pastelgreener <94100684+Pastelgreener@users.noreply.github.com>
2021-12-11 14:31:33 +0900

    Update Communication.java

--- smarthome/communication/Communication.java
+++ smarthome/communication/Communication.java
@@ -6,34 +6,51 @@ import com.google.gson.Gson;
 import java.io.*;
 import java.net.Socket;
 
-public class Communication implements Runnable {
-    private String ip = "localhost";
-    private Socket socket;
-    private int PORT;
-    private BufferedReader in = null;
-    private PrintWriter out = null;
-    private Gson gson = new Gson();
-    JsonMessage message;
-    Thread thread;
+    public class Communication {
+        private String ip = "localhost";
+        private Socket socket;
+        private int PORT;
+        private BufferedReader in = null;
+        private PrintWriter out = null;
+        private Gson gson = new Gson();
+        JsonMessage message;
 
-    public void connectServer() {
-        try {
-            socket = new Socket(ip, PORT);
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
-            JsonMessage message = new JsonMessage();
-            message.setId("smartHome");
-            message.setData("register");
-            out.println(gson.toJson(message));
-
-            thread = new Thread(this);
-            thread.start();
-        } catch (IOException e) {
-            e.printStackTrace();
+        public Communication() {
+            this(40000);
         }
-    }
-    @Override
-    public void run() {
+
+        public Communication(int PORT) {
+            this.PORT = PORT;
+        }
+
+        public void connectServer() {
+            try {
+                socket = new Socket(ip, PORT);
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+                message = new JsonMessage();
+                message.setId("smartHome");
+                message.setData("register");
+                out.println(gson.toJson(message));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void open() {
+            message.setData("open");
+            out.println(gson.toJson(message));
+        }
+
+        public void close() {
+            message.setData("close");
+            out.println(gson.toJson(message));
+        }
+
+        public void stop() {
+            message.setData("stop");
+            out.println(gson.toJson(message));
+        }
 
     }
-}
