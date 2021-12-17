b8a941b6b9aa1f118b79c0bf769bb2973d92e974
kyuhyeongkwon <akcps1tp@naver.com>
2021-12-18 02:19:04 +0900

    name changed

--- smarthome/SmartHomeSystemMain.java
+++ smarthome/SmartHomeSystemMain.java
@@ -1,7 +1,7 @@
 package FinalProject.smarthome;
 import FinalProject.smarthome.communication.Communication;
 
-public class Testmain {
+public class SmartHomeSystemMain {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 
